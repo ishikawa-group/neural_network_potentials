37a8a387fc189b9f41a3ccfe7c36acca311c4046
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2024-05-08 14:56:20 +0900

    update

--- calculate.py
+++ calculate.py
@@ -8,22 +8,11 @@ from ase.calculators.emt import EMT
 import os
 import numpy as np
 
-#checkpoint_path = "./gemnet_t_direct_h512_all.pt"
-#checkpoint_path = "/Users/ishi/opencatalystproject_new/checkpoints/2024-04-17-09-12-48-S2EF-example/best_checkpoint.pt"
-#checkpoint = "gndt_oc22_all_s2ef.pt"
-checkpoint = "schnet_all_large.pt"
-checkpoint = "dimenetpp_all.pt"
-checkpoint = "gemnet_t_direct_h512_all.pt"
-checkpoint = "gemnet_oc_base_s2ef_all_md.pt"
-checkpoint = "painn_h512_s2ef_all.pt"
+# odac23 does not work well?
 checkpoint = "gndt_oc22_all_s2ef.pt"
-#checkpoint = "gnoc_oc22_all_s2ef.pt"
-
-# odac23 --- does not work well?
-
 checkpoint_dir = "./downloaded_checkpoints/oc22/"
 checkpoint_path = checkpoint_dir + checkpoint
-#config_yml = "gemnet-dT.yml"
+
 trainer = "forces"  # for S2EF
 
 # Construct a sample structure
@@ -41,16 +30,12 @@ adslab.center(vacuum=13.0, axis=2)
 adslab.set_pbc(True)
 
 # Define the calculator
-calc = OCPCalculator(checkpoint_path=checkpoint_path)
-#calc = OCPCalculator(checkpoint_path=checkpoint_path, trainer=trainer)
-#calc = OCPCalculator(checkpoint_path=checkpoint_path, config_yml=config_yml)
+calc = OCPCalculator(checkpoint_path=checkpoint_path, trainer=trainer)
 #calc = EMT()
 
 # Set up the calculator
 adslab.calc = calc
 
-os.makedirs("test_traj", exist_ok=True)
-opt = BFGS(adslab, trajectory="test_traj/test.traj")
+opt = BFGS(adslab, trajectory="test.traj")
 
 opt.run(fmax=0.05, steps=100)
-
